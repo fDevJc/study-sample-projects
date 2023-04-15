4ec9d1018a6855afe6e87bcb8718c4c1ebd7249a
fDevJc <fdevjc@gmail.com>
2023-04-15 18:32:33 +0900

    module-common의 스프링빈 주입(scanBasePackages를 수동으로 설정)

--- study-module-sample/module-api/src/main/java/com/jc/moduleapi/ModuleApiApplication.java
+++ study-module-sample/module-api/src/main/java/com/jc/moduleapi/ModuleApiApplication.java
@@ -1,9 +1,9 @@
-package com.jc;
+package com.jc.moduleapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.jc.moduleapi","com.jc.modulecommon"})
 public class ModuleApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ModuleApiApplication.class, args);
