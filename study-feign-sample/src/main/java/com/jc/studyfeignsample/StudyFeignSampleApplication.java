fe511157ee5e3c0b75fae3468a3eb35d910efdfa
fDevJc <fdevjc@gmail.com>
2023-04-16 17:59:29 +0900

    feign init

---
+++ study-feign-sample/src/main/java/com/jc/studyfeignsample/StudyFeignSampleApplication.java
@@ -0,0 +1,13 @@
+package com.jc.studyfeignsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyFeignSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyFeignSampleApplication.class, args);
+	}
+
+}
---
+++ study-feign-sample/src/test/java/com/jc/studyfeignsample/StudyFeignSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jc.studyfeignsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyFeignSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
