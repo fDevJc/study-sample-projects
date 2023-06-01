0b571582963dff20e911e0923ced408dbee73cab
fDevJc <fdevjc@gmail.com>
2023-06-01 17:36:24 +0900

    init commit

---
+++ study-tdd-sample/src/main/java/com/jc/studytddsample/StudyTddSampleApplication.java
@@ -0,0 +1,13 @@
+package com.jc.studytddsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyTddSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyTddSampleApplication.class, args);
+	}
+
+}
