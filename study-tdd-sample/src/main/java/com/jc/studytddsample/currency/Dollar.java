f57269fbf27f50e998ac08bd63b12e83c54b1ab9
fDevJc <fdevjc@gmail.com>
2023-06-01 17:41:10 +0900

    곱하기 실패 테스트 작성

---
+++ study-tdd-sample/src/main/java/com/jc/studytddsample/currency/Dollar.java
@@ -0,0 +1,13 @@
+package com.jc.studytddsample.currency;
+
+public class Dollar {
+	public int amount;
+
+	public Dollar(int i) {
+
+	}
+
+	public void times(int i) {
+
+	}
+}
---
+++ study-tdd-sample/src/test/java/com/jc/studytddsample/currency/chapter01.java
@@ -0,0 +1,14 @@
+package com.jc.studytddsample.currency;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class chapter01 {
+	@Test
+	void testMultiplication() throws Exception {
+		Dollar five = new Dollar(5);
+		five.times(2);
+
+		Assertions.assertThat(five.amount).isEqualTo(10);
+	}
+}
