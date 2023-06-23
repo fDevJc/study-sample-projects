32b6e7002446201b05ff9c2ce5a54c892d76c68e
fDevJc <fdevjc@gmail.com>
2023-06-23 15:56:33 +0900

    init commit

---
+++ study-algorithm-sample/src/main/java/com/jc/studyalgorithmsample/StudyAlgorithmSampleApplication.java
@@ -0,0 +1,13 @@
+package com.jc.studyalgorithmsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyAlgorithmSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyAlgorithmSampleApplication.class, args);
+	}
+
+}
